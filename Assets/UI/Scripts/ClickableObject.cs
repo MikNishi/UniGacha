7da7a8c41317661aad01e2ed1c8a88d6ad8f12f3
MikNishi <miknishi1584@gmail.com>
2025-05-29 02:26:18 +0700

    clickable object script

---
+++ Assets/UI/Scripts/ClickableObject.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickableObject : MonoBehaviour, IPointerClickHandler
+{
+    public enum ActionType
+    {
+        OpenMenu,
+        StartRoguelike,
+        OpenGacha,
+        Custom
+    }
+
+    public ActionType action;
+
+    public GameObject targetPanel; // Используется, если нужно показывать/скрывать панель
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        switch (action)
+        {
+            case ActionType.OpenMenu:
+                if (targetPanel != null)
+                    targetPanel.SetActive(!targetPanel.activeSelf);
+                break;
+            case ActionType.StartRoguelike:
+                Debug.Log("Запуск мини-игры");
+                // Загрузить сцену или активировать объект
+                break;
+            case ActionType.OpenGacha:
+                Debug.Log("Открыть гачу");
+                break;
+            case ActionType.Custom:
+                Debug.Log("Пользовательская логика");
+                break;
+        }
+    }
+}
+     
